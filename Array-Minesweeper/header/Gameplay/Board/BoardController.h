e86ddb929f1497aec87af8a1528de4b51584a05b
meteoralpha23 <luckycreed123@gmail.com>
2024-12-27 17:45:41 +0530

    CREATING Board Controller and Board Service

---
+++ Array-Minesweeper/header/Gameplay/Board/BoardController.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <sfml/Graphics.hpp>
+
+
+namespace Gameplay
+{
+    namespace Board
+    {
+        class BoardController
+        {
+        public:
+            static const int number_of_rows = 9;
+            static const int number_of_colums = 9;
+            static const int mines_count = 8;
+
+            BoardController();
+            ~BoardController();
+
+           
+
+            void initialize();
+            void update();
+            void render();
+            void reset();
+            void move();
+          
+
+        private:
+            void createBoard();
+            void destroy();
+            void deleteBoard();
+        };
+    }
+}
--- Array-Minesweeper/header/Global/ServiceLocator.h
+++ Array-Minesweeper/header/Global/ServiceLocator.h
@@ -3,7 +3,7 @@
 #include "../../header/Event/EventService.h"
 #include "../../header/UI/UIService.h"
 #include "../../header/Sound/SoundService.h"
-
+#include "../../header/Gameplay/Board/BoardService.h"
 namespace Global
 {
     class ServiceLocator
@@ -13,6 +13,7 @@ namespace Global
         Graphics::GraphicService* graphic_service;
         Sound::SoundService* sound_service;
         UI::UIService* ui_service;
+        Gameplay::Board::BoardService* board_service;
 
         ServiceLocator();
         ~ServiceLocator();
@@ -31,6 +32,7 @@ namespace Global
         Graphics::GraphicService* getGraphicService();
         Sound::SoundService* getSoundService();
         UI::UIService* getUIService();
+        Gameplay::Board::BoardService* getBoardService();
         void deleteServiceLocator();
     };
 }
